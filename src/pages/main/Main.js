653db48b581226dda4b0e20147a6d29f56083f92
someonecantcode <s-btea@lwsd.org>
2024-06-09 06:53:20 -0700

    gh pages

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -2,13 +2,13 @@ import React from 'react';
 import AboutUs from "./aboutUs/AboutUs";
 import ContactUs from "./contactUs/ContactUs";
 import PlannerRank from "./plannerRank/PlannerRank";
+import SubmissionData from ".submissionData/SubmissionData"
 
 
 function Main() {
     return (
         <div className="main-page">
-            <AboutUs />
-            <ContactUs />
+
             <PlannerRank />
         </div>
     );
